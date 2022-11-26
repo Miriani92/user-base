d5b99edd94138d1abea27006a390b624bebb34ba
Miriani92 <mirian.tsintsadze1992@gmail.com>
2022-11-26 22:30:57 +0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './pages/category/category.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserStatusComponent } from './pages/user-status/user-status.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'userstatus', component: UserStatusComponent },
+  { path: 'userlist', component: UserListComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: '', redirectTo: '/userstatus', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { UserStatusComponent } from './pages/user-status/user-status.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    UserStatusComponent,
+    UserListComponent,
+    DetailsComponent,
+    NavbarComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
