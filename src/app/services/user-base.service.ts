6d273c2bb4c90e65b68fdaf341979a43db46a1b2
Miriani92 <mirian.tsintsadze1992@gmail.com>
2022-12-01 10:12:14 +0400

    add models

--- src/app/services/user-base.service.ts
+++ src/app/services/user-base.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UserData } from '../modules/userBaseApi/userBaseApi.module';
+import { UserData } from '../models/user.model';
 
 const url = {
   user: 'http://localhost:5000/api/v1/users',
@@ -14,6 +14,11 @@ const url = {
 })
 export class UserBaseService {
   constructor(private http: HttpClient) {}
+  /*  addUser():Observable<UserData>{
+
+  }
+*/
+
   getUserList(): Observable<UserData> {
     return this.http.get<UserData>(url.user);
   }
